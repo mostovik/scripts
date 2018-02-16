86028650a0cd354e9a346c78c9888521c21c4593
mostovik <35977395+mostovik@users.noreply.github.com>
2018-02-16 08:01:37 -0800

    Add files via upload

---
+++ case.sh
@@ -0,0 +1,15 @@
+!/bin/bash
+#nn=`cat /etc/issue | cut -d' ' -f1,2`
+#case  $nn in
+#"Ubuntu 14.04.5")
+#    dpkg -i pckg/keychain_2.7.1-1_1404all.deb
+#echo pckg/keychain_2.7.1-1_1404all.deb
+#   ;;
+#"Ubuntu 12.04.5")
+    #dpkg -i pckg/keychain_2.7.1-1_1204all.deb
+#echo pckg/keychain_2.7.1-1_1404all.deb    
+#;;
+#*)
+#    echo nada
+#    ;;
+#esac
