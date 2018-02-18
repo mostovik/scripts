380fec4f1518fd8398c740d0073f01ab9290f5b2
alxsycc <mostovikn@bk.ru>
2018-02-18 15:46:33 -0200

    atualizacao escola.java, written by alxsy mostovik 18/02/2018_03:46:33 PM

---
+++ bash/bin.sh
@@ -0,0 +1,3 @@
+git add -A
+git commit -S -m "$*, written by alxsy mostovik `date +%d/%m/%Y_%r`"
+git push
---
+++ bash/java/escola.java
@@ -0,0 +1,38 @@
+class escola{
+public static void main(String[] args){
+aluno al=new aluno();
+al.setNome("alex");
+System.out.println("bem vindo "+al.getNome());
+professor pr=new professor();
+pr.setNome("Andre");
+System.out.println("sou aluno de "+pr.getNome());
+}
+}
+
+
+
+class aluno{
+String nome;
+
+public void setNome(String nome){
+this.nome=nome;
+}
+
+public String getNome(){
+return this.nome;
+}
+}
+
+
+
+class professor{
+String nome;
+public void setNome(String nome){
+this.nome=nome;
+}
+
+public String getNome(){
+return this.nome;
+}
+
+}
