a949b3c7800852ef59e8bd1fbeba8b4333f054b1
alxsycc <mostovikn@bk.ru>
2018-02-22 12:16:44 -0200

    correcao cantinsa.cs, written by alxsy mostovik 22/02/2018_12:16:44 PM

--- bash/csharp/cantina.cs
+++ bash/csharp/cantina.cs
@@ -3,8 +3,11 @@ namespace cant{
 class cantina{
 public static void Main(String[] n){
 bool zap=true;
+String resp;
+ produto pr = new produto();
+ Console.Clear();
   do{
-  Console.WriteLine("bem vindo a AmartRango\nescolha o seu cardápio");
+Console.WriteLine("bem vindo a AmartRango\nescolha o seu cardápio");
 Console.WriteLine(" ########################################");
 Console.WriteLine("#1-pastel 2.20\t\t\t#");
 Console.WriteLine("#2-hamburguer 3.00\t\t\t#");
@@ -12,21 +15,44 @@ Console.WriteLine("#3-coxinha 2.00\t\t\t#");
 Console.WriteLine("#4-esfirra 3.50\t\t\t#");
 Console.WriteLine("#5-Finalizar\t\t\t#");
 Console.WriteLine(" ########################################");
-String resp=Console.ReadLine();
-if(resp=="5"){
-zap=false; 
-  produto pr = new produto();
-//Console.WriteLine("okj");
-pr.Preco=10;
-pr.Code="0110011110101";
-Console.WriteLine("codigo da compra:"+pr.Code);
-Console.WriteLine("total compra:"+String.Format("{0:0.00}",pr.Preco));
-Console.WriteLine("compra com juros:"+String.Format("{0:0.00}",pr.calcular ));
-Console.WriteLine("codigo da compra:%f",pr.Preco);
+resp=Console.ReadLine();
+//pr.Preco+=float.Parse(resp);
+
+    
+    
+    switch(resp){
+      case "1":
+        pr.preco+=2.2F;
+           break;
+
+      case "2":
+        pr.preco=3;
+           break;
+     
+      case "3":
+        pr.preco=2;
+           break;
+     
+      case "4":
+        pr.preco=3;
+           break;
+     
+      case "5":
+         zap=false; 
+           break;
+     
+      default:
+           break;
+     
   }
-}
+    Console.Clear();
+    }
   while(zap);
-  
+  //produto pr = new produto();
+ //pr.Code="0110011110101";
+Console.WriteLine("preco da compra:"+String.Format("{0:0.00}",pr.Preco));
+//Console.WriteLine("total compra:"+String.Format("{0:0.00}",pr.Preco));
+Console.WriteLine("compra com juros:"+String.Format("{0:0.00}",pr.calcular ));
   //fim class main
 }}}
 
@@ -45,8 +71,8 @@ Console.WriteLine("codigo da compra:%f",pr.Preco);
 
 
 class produto{
-String desc,code;
-float preco;
+public String desc,code;
+public float preco;
 
 String Desc{
 set{desc=value;}
