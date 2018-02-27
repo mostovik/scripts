a36fd5b614eeee1defd838a1e4211ac570f476a8
alxsycc <mostovikn@bk.ru>
2018-02-27 15:20:09 -0200

    adcionando settings Ubuntu web server, written by alxsy mostovik 27/02/2018_03:20:09 PM

---
+++ UbuntuServerWeb.sh
@@ -0,0 +1,35 @@
+#atualizar packote
+apt-get update -y
+
+apt-get upgrade -y
+
+#install apache2
+apt-get -y install apache2
+
+#install php
+apt-get install -y php5 libapache2-mod-php5
+
+#resiniciar apache2
+service apache2 restart
+
+#instalar mysql
+apt-get install -y mysql-server
+
+#instalar phpmyadmin
+apt-get install php5-mysql phpmyadmin libapache2-mod-auth-mysql -y
+
+#link phpmyadmin apache2
+ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
+
+chmod 777 -R /var/www/
+ln -s /var/www/ .
+
+#instalando lamp
+#apt-get install lamp^ phpmyadmin -y
+
+
+#instalando xampp
+#sudo add-apt-repository ppa:upubuntu-com/web -y
+#apt-get update -y
+#apt-get install xampp -y
+#sudo /opt/lampp/lampp start
